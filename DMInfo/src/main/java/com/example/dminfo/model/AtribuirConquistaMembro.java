1907037abe6aab2fec0db10d5c36cc7e03cd724a
Mateus Maciel <matteus6537@hotmail.com>
2025-10-24 08:09:16 -0300

    ADD Model
    Adiciona AtribuirConquistaMembro.java

---
+++ DMInfo/src/main/java/com/example/dminfo/model/AtribuirConquistaMembro.java
@@ -0,0 +1,90 @@
+package com.example.dminfo.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "atribuir_conquista_membro")
+public class AtribuirConquistaMembro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_atribuir_conquista")
+    private int id_atribuir_conquista;
+
+    @Column(name = "id_admin")
+    private int id_admin;
+
+    @Column(name = "id_membro")
+    private int id_membro;
+
+    @Column(name = "id_conquista")
+    private int id_conquista;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data")
+    private Date data;
+
+    @Column(name = "observacao")
+    private String observacao;
+
+    public AtribuirConquistaMembro() {
+    }
+
+    public AtribuirConquistaMembro(int id_atribuir_conquista, int id_admin, int id_membro, int id_conquista, Date data, String observacao) {
+        this.id_atribuir_conquista = id_atribuir_conquista;
+        this.id_admin = id_admin;
+        this.id_membro = id_membro;
+        this.id_conquista = id_conquista;
+        this.data = data;
+        this.observacao = observacao;
+    }
+
+    public int getId_atribuir_conquista() {
+        return id_atribuir_conquista;
+    }
+
+    public void setId_atribuir_conquista(int id_atribuir_conquista) {
+        this.id_atribuir_conquista = id_atribuir_conquista;
+    }
+
+    public int getId_admin() {
+        return id_admin;
+    }
+
+    public void setId_admin(int id_admin) {
+        this.id_admin = id_admin;
+    }
+
+    public int getId_membro() {
+        return id_membro;
+    }
+
+    public void setId_membro(int id_membro) {
+        this.id_membro = id_membro;
+    }
+
+    public int getId_conquista() {
+        return id_conquista;
+    }
+
+    public void setId_conquista(int id_conquista) {
+        this.id_conquista = id_conquista;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+}

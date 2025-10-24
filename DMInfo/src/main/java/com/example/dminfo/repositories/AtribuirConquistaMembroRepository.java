5bdfc6a6c78e7ce082d2fe5f2333cfecf48b98b8
Mateus Maciel <matteus6537@hotmail.com>
2025-10-24 08:36:18 -0300

    ADD to Repository
    Adiciona repositorios para as classes Conquista e AtribuirConquista

---
+++ DMInfo/src/main/java/com/example/dminfo/repositories/AtribuirConquistaMembroRepository.java
@@ -0,0 +1,6 @@
+package com.example.dminfo.repositories;
+
+import com.example.dminfo.model.AtribuirConquistaMembro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtribuirConquistaMembroRepository extends JpaRepository<AtribuirConquistaMembro, Integer> { }
---
+++ DMInfo/src/main/java/com/example/dminfo/repositories/ConquistaRepository.java
@@ -0,0 +1,7 @@
+package com.example.dminfo.repositories;
+
+
+import com.example.dminfo.model.Conquista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConquistaRepository extends JpaRepository<Conquista, Integer> { }

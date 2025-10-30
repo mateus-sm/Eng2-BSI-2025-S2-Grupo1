d444abb1cdaef2a31415081660c4ec40b5aa39cd
Lucas Ataide Malagueta <lucasataidepq@gmail.com>
2025-10-29 23:51:15 -0300

    Update LancarPagMensalidade

--- DMInfo/src/main/java/com/example/dminfo/controller/LancarPagamentoMensalidadeController.java
+++ DMInfo/src/main/java/com/example/dminfo/controller/LancarPagamentoMensalidadeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-    @RequestMapping("/lancar-pagamento-mensalidade")
+    @RequestMapping("/mensalidade")
 public class LancarPagamentoMensalidadeController {
     @Autowired LancarPagamentoMensalidadeService LPMservice;
 

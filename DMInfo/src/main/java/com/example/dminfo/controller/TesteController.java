862ba8ebd5d73baf42088bcf9fd9d168873a1f16
Eduardo Carbonari Silva <ducarbosilva@hotmail.com>
2025-11-01 23:09:15 -0300

    DELETE Removido TesteService

--- DMInfo/src/main/java/com/example/dminfo/controller/TesteController.java
+++ DMInfo/src/main/java/com/example/dminfo/controller/TesteController.java
@@ -1,11 +1,8 @@
 package com.example.dminfo.controller;
 
 import com.example.dminfo.model.Membro;
-import com.example.dminfo.services.TesteService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 //@RestController
 //@RequestMapping(value = "test")
